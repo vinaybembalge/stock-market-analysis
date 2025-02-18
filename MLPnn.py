e4709852d16f4299f40f0656326ffe6bc7e07b07
vinaybembalge <21b080033@iitb.ac.in>
2025-02-19 01:06:45 +0530

    Update MLPnn.py

--- MLPnn.py
+++ MLPnn.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-# This is just a practice test
-
-# Split dataset into training (66%) and test (33%) set
+# Load dataset and split into training (66%) and test (33%) sets
 filename = 'training_set.csv'
 raw_data = open(filename, 'rt')
 training_set = np.loadtxt(raw_data, delimiter=",")
@@ -20,120 +18,109 @@ filename = 'test_labels.csv'
 raw_data = open(filename, 'rt')
 test_labels = np.loadtxt(raw_data, delimiter=",")
 
-print("Dataset ready.") 
+print("Dataset successfully loaded.")
 
-# Parameters
-learning_rate   = 0.01 
-mini_batch_size = 10
-training_epochs = 5000
-display_step    = 1000
+# Hyperparameters
+learning_rate = 0.01 
+batch_size = 10
+num_epochs = 5000
+log_interval = 1000
 
-# Network Parameters
-n_hidden_1  = 64    # 1st hidden layer of neurons
-n_hidden_2  = 32    # 2nd hidden layer of neurons
-n_hidden_3  = 16    # 3rd hidden layer of neurons
-n_input     = 50   # number of features after LSA
+# Neural Network Specifications
+num_neurons_1 = 64    # First hidden layer
+num_neurons_2 = 32    # Second hidden layer
+num_neurons_3 = 16    # Third hidden layer
+num_features = 50     # Input layer size
 
-# Tensorflow Graph input
+# Define placeholders for input and output
 with tf.name_scope("inputs"):
-    x = tf.placeholder(tf.float64, shape=[None, n_input], name="x-data")
-    y = tf.placeholder(tf.float64, shape=[None, 1], name="y-labels")
+    x = tf.placeholder(tf.float64, shape=[None, num_features], name="input_data")
+    y = tf.placeholder(tf.float64, shape=[None, 1], name="output_labels")
 
-print("Creating model.")
+print("Initializing the neural network model.")
 
-# Create model
-def multilayer_perceptron(x, weights):
-    with tf.name_scope("layer"):
+# Define the neural network architecture
+def mlp_model(x, weights):
+    with tf.name_scope("layers"):
         with tf.name_scope("weights"):
-            weights['h1'] = tf.Variable(tf.random_normal([n_input, n_hidden_1], name = "W1", dtype=np.float64))
-            tf.summary.histogram("layer_1_weights", weights['h1'])
-            weights['h2'] = tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], name = "W2", dtype=np.float64))
-            tf.summary.histogram("layer_2_weights", weights['h2'])
-            weights['h3'] = tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3], name = "W3", dtype=np.float64))
-            tf.summary.histogram("layer_3_weights", weights['h3'])           
-            weights['out'] = tf.Variable(tf.random_normal([n_hidden_3, 1], name = "W4", dtype=np.float64))
-            tf.summary.histogram("output_layer_weights", weights['out'])            
-        with tf.name_scope("inputs"):
-            # First hidden layer with SIGMOID activation
-            layer_1 = tf.matmul(x, weights['h1'])
-            # Second hidden layer with SIGMOID activation
-            layer_2 = tf.matmul(layer_1, weights['h2'])
-            # Third hidden layer with SIGMOID activation
-            layer_3 = tf.matmul(layer_2, weights['h3'])
-            # Output layer with SIGMOID activation
-            out_layer = tf.matmul(layer_3, weights['out'])
+            weights['h1'] = tf.Variable(tf.random_normal([num_features, num_neurons_1], dtype=np.float64), name="W1")
+            tf.summary.histogram("layer1_weights", weights['h1'])
+            weights['h2'] = tf.Variable(tf.random_normal([num_neurons_1, num_neurons_2], dtype=np.float64), name="W2")
+            tf.summary.histogram("layer2_weights", weights['h2'])
+            weights['h3'] = tf.Variable(tf.random_normal([num_neurons_2, num_neurons_3], dtype=np.float64), name="W3")
+            tf.summary.histogram("layer3_weights", weights['h3'])
+            weights['out'] = tf.Variable(tf.random_normal([num_neurons_3, 1], dtype=np.float64), name="W4")
+            tf.summary.histogram("output_weights", weights['out'])
         
-        layer_1 = tf.nn.sigmoid(layer_1) 
-        tf.summary.histogram("layer_1_outputs", layer_1)
-        layer_2 = tf.nn.sigmoid(layer_2)
-        tf.summary.histogram("layer_2_outputs", layer_2)
-        layer_3 = tf.nn.sigmoid(layer_3)
-        tf.summary.histogram("layer_3_outputs", layer_3)
-        out_layer = tf.nn.sigmoid(out_layer)
-        tf.summary.histogram("output_layer_outputs", out_layer)
-    return out_layer
+        # Compute activations for each layer
+        with tf.name_scope("activations"):
+            layer1 = tf.nn.sigmoid(tf.matmul(x, weights['h1']))
+            tf.summary.histogram("layer1_output", layer1)
+            layer2 = tf.nn.sigmoid(tf.matmul(layer1, weights['h2']))
+            tf.summary.histogram("layer2_output", layer2)
+            layer3 = tf.nn.sigmoid(tf.matmul(layer2, weights['h3']))
+            tf.summary.histogram("layer3_output", layer3)
+            output_layer = tf.nn.sigmoid(tf.matmul(layer3, weights['out']))
+            tf.summary.histogram("final_output", output_layer)
+        
+    return output_layer
 
-# Layer weights, should change them to see results
+# Initialize weight variables
 weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], dtype=np.float64)),       
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], dtype=np.float64)),
-    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3],dtype=np.float64)),
-    'out': tf.Variable(tf.random_normal([n_hidden_3, 1], dtype=np.float64))
+    'h1': tf.Variable(tf.random_normal([num_features, num_neurons_1], dtype=np.float64)),       
+    'h2': tf.Variable(tf.random_normal([num_neurons_1, num_neurons_2], dtype=np.float64)),
+    'h3': tf.Variable(tf.random_normal([num_neurons_2, num_neurons_3], dtype=np.float64)),
+    'out': tf.Variable(tf.random_normal([num_neurons_3, 1], dtype=np.float64))
 }
 
-# Construct model
-pred = multilayer_perceptron(x, weights)
+# Construct the model
+predictions = mlp_model(x, weights)
 
-# Define loss and optimizer
-with tf.name_scope("loss"):
-    cost = tf.nn.l2_loss(pred-y,name="squared_error_cost")
-    tf.summary.scalar("loss", cost)
-with tf.name_scope("train"):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+# Define loss function and optimization method
+with tf.name_scope("loss_function"):
+    loss = tf.nn.l2_loss(predictions - y, name="mean_squared_error")
+    tf.summary.scalar("training_loss", loss)
 
-# Initializing the variables
+with tf.name_scope("optimizer"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+# Initialize variables
 init = tf.global_variables_initializer()
 
-print("Model ready.")
+print("Model successfully defined.")
 
-# Launch the graph
+# Begin training session
 with tf.Session() as sess:
-    
-    merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter("logs/", sess.graph)
+    merged_summary = tf.summary.merge_all()
+    summary_writer = tf.summary.FileWriter("logs/", sess.graph)
     
     sess.run(init)
+    print("Training process started.")
 
-    print("Starting Training.")
+    # Training loop
+    for epoch in range(num_epochs):
+        batch_x = training_set[batch_size * epoch:batch_size * (epoch + 1)]
+        batch_y = training_labels[batch_size * epoch:batch_size * (epoch + 1)]
+        batch_y = np.array(batch_y).reshape(len(batch_y), 1)
+        _, loss_value = sess.run([optimizer, loss], feed_dict={x: batch_x, y: batch_y})
 
-    # Training cycle
-    for epoch in range(training_epochs):
-        #avg_cost = 0.
-        # minibatch loading
-        minibatch_x = training_set[mini_batch_size*epoch:mini_batch_size*(epoch+1)]
-        minibatch_y = training_labels[mini_batch_size*epoch:mini_batch_size*(epoch+1)]
-        # Run optimization op (backprop) and cost op
-        minibatch_Y = np.array(minibatch_y).reshape(len(minibatch_y),1)
-        _, c = sess.run([optimizer, cost], feed_dict={x: minibatch_x, y: minibatch_Y})
+        avg_loss = loss_value / (batch_x.shape[0] + 0.01)
 
-        # Compute average loss
-        avg_cost = c / (minibatch_x.shape[0]+0.01)
-
-        # Display logs per epoch
-        if (epoch) % display_step == 0:
-            print("Epoch:", '%05d' % (epoch), "Training accuracy=", "{:.9f}".format((1-avg_cost)*100))
-        if (epoch) % 50 == 0:    
-            result = sess.run(merged,feed_dict={x: minibatch_x, y: minibatch_Y})
-            writer.add_summary(result,epoch)
-
-    print("Optimization Finished!")
-
-    # Test model
-    # Calculate accuracy
-    test_error = tf.nn.l2_loss(pred-y,name="squared_error_test_cost")/test_set.shape[0]
-    test_labels_y = np.array(test_labels).reshape(len(test_labels),1)
-    print("Test Accuracy: ", 100-test_error.eval({x: test_set, y: test_labels_y})*100, "%")
+        # Display progress at intervals
+        if epoch % log_interval == 0:
+            print("Epoch:", '%05d' % (epoch), "Training accuracy:", "{:.9f}".format((1 - avg_loss) * 100))
+        
+        if epoch % 50 == 0:
+            summary = sess.run(merged_summary, feed_dict={x: batch_x, y: batch_y})
+            summary_writer.add_summary(summary, epoch)
     
+    print("Training complete!")
+
+    # Evaluate test performance
+    test_loss = tf.nn.l2_loss(predictions - y, name="test_loss") / test_set.shape[0]
+    test_labels = np.array(test_labels).reshape(len(test_labels), 1)
+    print("Test Accuracy:", 100 - test_loss.eval({x: test_set, y: test_labels}) * 100, "%")
+
     
     
     
